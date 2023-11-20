6be44e70ce37608b9abb3ffe67d618d07348aa91
Gareth Craig <l00129014@atu.ie>
2023-11-20 15:55:01 +0000

    Replaced the border/label template for the button alternative. Added new icons for the hub component which will be added to the landing page to allow for a stacked navigation.

--- EquityX/Pages/Landing.xaml.cs
+++ EquityX/Pages/Landing.xaml.cs
@@ -5,5 +5,25 @@ public partial class Landing : ContentPage
 	public Landing()
 	{
 		InitializeComponent();
+
+		ListView listView = new ListView();
 	}
+
+	public void Add_Money_Clicked(Object sender, EventArgs e)
+	{
+		Navigation.PushAsync(new EquityX.Pages.AddMoney());
+
+    }
+
+    public void Sell_Assets_Clicked(Object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new EquityX.Pages.SellAssets());
+
+    }
+
+    public void Withdraw_Clicked(Object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new EquityX.Pages.Withdraw());
+
+    }
 }
